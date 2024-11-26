c61ba3c6710fd862ead53b3e12a62f6bcc548a8f
PATEL MAHD.HUSEN IMTIYAZ <130433484+Husain012@users.noreply.github.com>
2024-11-26 10:43:18 +0530

    initial check in master branch

---
+++ Models/Contactmessage.cs
@@ -0,0 +1,15 @@
+﻿namespace DreamProperties.Models
+{
+    public class Contactmessage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
